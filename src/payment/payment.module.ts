ed644d739ef78eb2387467d6f55e9b2491fe8fb1
Erlan-Bam <erlanzh.gg@gmail.com>
2025-11-20 21:32:37 +0500

    feat: add @ton/crypto dependency and update payment module providers

--- src/payment/payment.module.ts
+++ src/payment/payment.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { TonService } from './services/ton.service';
 
 @Module({
-  providers: [PaymentService],
-  controllers: [PaymentController]
+  providers: [PaymentService, TonService],
+  controllers: [PaymentController],
 })
 export class PaymentModule {}
